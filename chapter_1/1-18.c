f91c04c4fc17367ab657467733b4352bd498c705
Nasef Khan <nasef@Asus-Debian>
2015-02-23 10:55:57 +0000

    Modified 1-18

--- chapter_1/1-18.c
+++ chapter_1/1-18.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
-#define MAXLINE 10 //maximum input size
+#define MAXLINE 100 //maximum input size
 
-/* This was one was hard */
+ /* Assuming that "entirely blank lines" means lines only containing '\n' */
 
 int getline_(char line[], int maxline);
 void copy(char to[], char from[]);
 void remove_trailing_spaces(int len, char string[]);
 
-
 int main() {
+    char line[MAXLINE]; 
     int len; 
     int max; 
     int c;
 
     max = 0;
     while ((len = getline_(line, MAXLINE)) > 0){
-        printf("%d\n", len);
-        if (len == 1 && line[0] == '\n'){
+        //Don't print lines only containing '\n'
+        if (len == 1 && line[0] == '\n'){ 
             line[0] = '\0';
         }
-        if (len >= MAXLINE-1 && line[len-1] != '\n'){ // Deal with buffer overflow
+        
+        // Deal with buffer overflow
+        if (len >= MAXLINE-1 && line[len-1] != '\n'){ 
             while ((c=getchar()) != '\n'){
                 ;
             }
         }
+
         if (len > 1) {
             remove_trailing_spaces(len, line);
         }
+
         printf("%s", line);
     }
     return 0;
@@ -62,6 +66,6 @@ void remove_trailing_spaces(int len, char string[]){
     for (i=len-1; (string[i] == ' ' || string[i]  == '\t' || string[i] == '\n'); i--){
         ;
     }
-    string[++i] = '*';
+    string[++i] = '\n';
     string[++i] = '\0';
 }
